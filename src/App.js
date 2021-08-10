3bc75f10ac23b0f5ea7aa8c667f0c793496bc801
Scott <starkey9000@gmail.com>
2021-08-09 23:49:35 -0400

    first commit. React setup and Material-UI addition

---
+++ src/App.js
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        
+      </footer>
+    </div>
+  );
+}
+
+export default App;
